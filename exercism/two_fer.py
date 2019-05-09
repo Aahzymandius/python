59d78d9d8765d160d30e0b9bd1c8bc782b84dba5
Aahzymandius <50278685+Aahzymandius@users.noreply.github.com>
2019-05-09 09:07:17 -0400

    cleaned up the code

--- exercism/two_fer.py
+++ exercism/two_fer.py
@@ -1,8 +1,5 @@
-def two_fer(name):
-    if name == "":
-      print("One for you and one for me")
-    else:
-      print("One for " + name + ", one for me")
+def two_fer(name='you'): 
+    print("One for " + name + ", one for me")
 
-name=input("What is your name?",)
+name=input("What is your name?",) 
 two_fer(name)
