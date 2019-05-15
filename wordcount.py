e88017bb7c222c187dd64948106a500a7f93b72b
Aahzymandius <50278685+Aahzymandius@users.noreply.github.com>
2019-05-15 15:40:17 -0400

    Update wordcount.py

--- wordcount.py
+++ wordcount.py
@@ -1,22 +1,20 @@
 ##Count the number of times each word in a text is used
 
 def remover(n,list)
-  i=0
-  length=len(list)
-  while x < length:
-    if list[x]==n:
-	  list.remove(list[i])
-	  length-=1
-	  continue
-	x+=1
+  for x in list:
+    if x == n:
+	list.remove(x)
 
 def wordcount(text):
+  stats = {}
   with open(text) as t:
     words=t.read().split()
     for i in words:
       count=words.count(i)
-      print(str(i) + " appears " + str(count) + " times")
+      print(i + " appears " + str(count) + " times")
+      stats[i] = count
       remover(i,words)
-	  
+  return stats
+		
 text=input("Enter path text: ",)
 wordcount(text)
