89247d35fea3a6e4269cb017778e5a0426437671
Aahzymandius <50278685+Aahzymandius@users.noreply.github.com>
2019-06-19 01:01:05 -0400

    Update pangram.py

--- exercism/pangram.py
+++ exercism/pangram.py
@@ -1,12 +1,8 @@
 def is_pangram(sentence):
-  count=0
-  for l in "abcdefghijklmnopqrstuvwxyz"
-    if l in sentence:
-      count+=1
-    else:
-      print("Not a pangram")
-  if count >= 26:
-    print("This is a pangram")
+  for l in "abcdefghijklmnopqrstuvwxyz":
+    if l not in sentence:
+      return("This is not a pangram")
+  return("This is a pangram")
     
-sentence=input(Which sentence would you like to test?,)
+sentence=input("Which sentence would you like to test?",)
 is_pangram(sentence)
